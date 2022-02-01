d4ca42d407db36b9a164a05cab42227d1a0d61e2
Valeria Edwards <valeriaedwards17@gmail.com>
2022-01-31 21:54:41 -0600

    Agregué la clase lista

---
+++ src/estructuras/Lista.java
@@ -0,0 +1,132 @@
+package estructuras;
+
+public class Lista {
+
+    private Nodo inicio;
+    private int tamanio;
+
+    public void Lista(){
+        this.inicio = null;
+        this.tamanio = 0;
+    }
+
+    public boolean esVacia(){
+        return inicio == null;
+    }
+
+    public int getTamanio(){
+        return tamanio;
+    }
+
+    public void agregarAlFinal(int valor){
+        // Define un nuevo nodo.
+        Nodo nuevo = new Nodo();
+        // Agrega al valor al nodo.
+        nuevo.setValor(valor);
+        // Consulta si la lista esta vacia.
+        if (esVacia()) {
+            // Inicializa la lista agregando como inicio al nuevo nodo.
+            inicio = nuevo;
+            // Caso contrario recorre la lista hasta llegar al ultimo nodo
+            //y agrega el nuevo.
+        } else{
+            // Crea ua copia de la lista.
+            Nodo aux = inicio;
+            // Recorre la lista hasta llegar al ultimo nodo.
+            while(aux.getSiguiente() != null){
+                aux = aux.getSiguiente();
+            }
+            // Agrega el nuevo nodo al final de la lista.
+            aux.setSiguiente(nuevo);
+        }
+        // Incrementa el contador de tamaño de la lista
+        tamanio++;
+    }
+
+    public void agregarAlInicio(int valor){
+        // Define un nuevo nodo.
+        Nodo nuevo = new Nodo();
+        // Agrega al valor al nodo.
+        nuevo.setValor(valor);
+        // Consulta si la lista esta vacia.
+        if (esVacia()) {
+            // Inicializa la lista agregando como inicio al nuevo nodo.
+            inicio = nuevo;
+            // Caso contrario va agregando los nodos al inicio de la lista.
+        } else{
+            // Une el nuevo nodo con la lista existente.
+            nuevo.setSiguiente(inicio);
+            // Renombra al nuevo nodo como el inicio de la lista.
+            inicio = nuevo;
+        }
+        // Incrementa el contador de tamaño de la lista.
+        tamanio++;
+    }
+
+    public boolean buscar(int referencia){
+        // Crea una copia de la lista.
+        Nodo aux = inicio;
+        // Bandera para indicar si el valor existe.
+        boolean encontrado = false;
+        // Recorre la lista hasta encontrar el elemento o hasta
+        // llegar al final de la lista.
+        while(aux != null && encontrado != true){
+            // Consulta si el valor del nodo es igual al de referencia.
+            if (referencia == aux.getValor()){
+                // Canbia el valor de la bandera.
+                encontrado = true;
+            }
+            else{
+                // Avansa al siguiente. nodo.
+                aux = aux.getSiguiente();
+            }
+        }
+        // Retorna el resultado de la bandera.
+        return encontrado;
+    }
+
+    public void removerPorReferencia(int referencia){
+        // Consulta si el valor de referencia existe en la lista.
+        if (buscar(referencia)) {
+            // Consulta si el nodo a eliminar es el pirmero
+            if (inicio.getValor() == referencia) {
+                // El primer nodo apunta al siguiente.
+                inicio = inicio.getSiguiente();
+            } else{
+                // Crea ua copia de la lista.
+                Nodo aux = inicio;
+                // Recorre la lista hasta llegar al nodo anterior
+                // al de referencia.
+                while(aux.getSiguiente().getValor() != referencia){
+                    aux = aux.getSiguiente();
+                }
+                // Guarda el nodo siguiente del nodo a eliminar.
+                Nodo siguiente = aux.getSiguiente().getSiguiente();
+                // Enlaza el nodo anterior al de eliminar con el
+                // sguiente despues de el.
+                aux.setSiguiente(siguiente);
+            }
+            // Disminuye el contador de tamaño de la lista.
+            tamanio--;
+        }
+    }
+
+    public void listar(){
+        // Verifica si la lista contiene elementoa.
+        if (!esVacia()) {
+            // Crea una copia de la lista.
+            Nodo aux = inicio;
+            // Posicion de los elementos de la lista.
+            int i = 0;
+            // Recorre la lista hasta el final.
+            while(aux != null){
+                // Imprime en pantalla el valor del nodo.
+                System.out.print(i + ".[ " + aux.getValor() + " ]" + " ->  ");
+                // Avanza al siguiente nodo.
+                aux = aux.getSiguiente();
+                // Incrementa el contador de la posión.
+                i++;
+            }
+        }
+    }
+}
